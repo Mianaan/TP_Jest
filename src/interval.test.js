1c2ad0063f564bd71c4267f9e4e09b5540d97174
Adèle Wagner <adele.wagner97@gmail.com>
2019-03-28 17:22:36 +0100

    file not up to date, replace overlaps tests by the latest ones

--- src/interval.test.js
+++ src/interval.test.js
@@ -5,31 +5,43 @@ describe('Overlaps', function () {
     test('Interval 1 overlaps interval 2 is true', () => {
         let int1 = new Interval(5,10);
         let int2 = new Interval(4,9);
-            expect(int1.overlaps(int2)).toBeTruthy();
-        });
+        expect(int1.overlaps(int2)).toBe(true);
+    });
 
     test('Interval 1 overlaps interval 2 (is a dot) is true', () => {
         let int1 = new Interval(14,16);
         let int2 = new Interval(15,15);
-        expect(int1.overlaps(int2)).toBeTruthy();
+        expect(int1.overlaps(int2)).toBe(true);
+    });
+
+    test('Interval 1 overlaps interval 2 (bis) is true', () => {
+        let int1 = new Interval(1,16);
+        let int2 = new Interval(5,30);
+        expect(int1.overlaps(int2)).toBe(true);
     });
 
     test('Interval 1 (is a dot) overlaps interval 2 (is a dot) is false', () => {
         let int1 = new Interval(15,15);
         let int2 = new Interval(15,15);
-        expect(int1.overlaps(int2)).toBeFalsy();
+        expect(int1.overlaps(int2)).toBe(false);
     });
 
     test('Interval 1 (is a dot) overlaps interval 2 is false', () => {
         let int1 = new Interval(15,15);
         let int2 = new Interval(15,20);
-        expect(int1.overlaps(int2)).toBeFalsy();
+        expect(int1.overlaps(int2)).toBe(false);
     });
 
     test('Interval 1 overlaps interval 2 is false', () => {
         let int1 = new Interval(3,30);
         let int2 = new Interval(30,35);
-        expect(int1.overlaps(int2)).toBeFalsy();
+        expect(int1.overlaps(int2)).toBe(false);
+    });
+
+    test('Interval 1 (bis) overlaps interval 2 is false', () => {
+        let int1 = new Interval(5,2);
+        let int2 = new Interval(3,5);
+        expect(int1.overlaps(int2)).toBe(false);
     });
 });
 
