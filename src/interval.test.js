061d7853d55bd197b94ed2668fae4f722f13296f
Adèle Wagner <adele.wagner97@gmail.com>
2019-03-28 17:05:16 +0100

    add 5 new tests for include function

--- src/interval.test.js
+++ src/interval.test.js
@@ -31,6 +31,37 @@ describe('Overlaps', function () {
         let int2 = new Interval(30,35);
         expect(int1.overlaps(int2)).toBeFalsy();
     });
+});
 
+describe('Includes', function () {
 
+    test('Interval 1 includes interval 2 is true', () => {
+        let int1 = new Interval(5,10);
+        let int2 = new Interval(6,9);
+        expect(int1.includes(int2)).toBe(true);
+    });
+
+    test('Interval 2 includes interval 1 is false', () => {
+        let int1 = new Interval(15,20);
+        let int2 = new Interval(10,30);
+        expect(int1.includes(int2)).toBe(false);
+    });
+
+    test('Interval 1 (is a dot) includes interval 2 (is a dot) is true', () => {
+        let int1 = new Interval(15,15);
+        let int2 = new Interval(15,15);
+        expect(int1.includes(int2)).toBe(true);
+    });
+
+    test('Interval 1 (bis) includes interval 2 is false', () => {
+        let int1 = new Interval(5,2);
+        let int2 = new Interval(3,5);
+        expect(int1.includes(int2)).toBe(false);
+    });
+
+    test('Interval 1 includes interval 2 is false', () => {
+        let int1 = new Interval(4,8);
+        let int2 = new Interval(10,16);
+        expect(int1.includes(int2)).toBe(false);
+    });
 });
