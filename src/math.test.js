bd75d7732b05794c8fb777c6ba9737e1b297f41a
Adèle Wagner <adele.wagner97@gmail.com>
2019-04-02 12:11:35 +0200

    change tests and type of test

--- src/math.test.js
+++ src/math.test.js
@@ -96,16 +96,16 @@ describe('FizzBuzz', function () {
     });
 
     test.each([
-        [2, 2],
-        [5, "Buzz"],
-        [8, 8],
-        [6, "Fizz"],
-        [30, "FizzBuzz"],
-        [97, 97],
+        [2, [1, 2]],
+        [5, [1, 2, "Fizz", 4, "Buzz"]],
+        [8, [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8]],
+        [6, [1, 2, "Fizz", 4, "Buzz", "Fizz"]],
+        [30, [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz", 16, 17, "Fizz", 19, "Buzz", "Fizz", 22, 23, "Fizz", "Buzz", 26, "Fizz", 28, 29, "FizzBuzz"]],
+        [97, [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz", 16, 17, "Fizz", 19, "Buzz", "Fizz", 22, 23, "Fizz", "Buzz", 26, "Fizz", 28, 29, "FizzBuzz", 31, 32, "Fizz", 34, "Buzz", "Fizz", 37, 38, "Fizz", "Buzz", 41, "Fizz", 43, 44, "FizzBuzz", 46, 47, "Fizz", 49, "Buzz", "Fizz", 52, 53, "Fizz", "Buzz", 56, "Fizz", 58, 59, "FizzBuzz", 61, 62, "Fizz", 64, "Buzz", "Fizz", 67, 68, "Fizz", "Buzz", 71, "Fizz", 73, 74, "FizzBuzz", 76, 77, "Fizz", 79, "Buzz", "Fizz", 82, 83, "Fizz", "Buzz", 86, "Fizz", 88, 89, "FizzBuzz", 91, 92, "Fizz", 94, "Buzz", "Fizz", 97]],
     ])(
         'FizzBuzz %i equals to %i',
         (n, expected) => {
-            expect(Util.fizzBuzz(n)).toBe(expected);
+            expect(Util.fizzBuzz(n)).toEqual(expected);
         }
     );
 });
